5c3044927d12d5919297e8e92a41a60d25e14866
Kyle Givler <xvegan88x@gmail.com>
2023-08-07 19:55:11 -0400

    "Elevated world"

--- FunAndProfitExamples/ElevatedWorldLiftN.fsx
+++ FunAndProfitExamples/ElevatedWorldLiftN.fsx
@@ -26,6 +26,24 @@ module Option =
     let lift4 f x y z w =
         f <!> x <*> y <*> z <*> w
 
+    // Lift2 can be used as an alternative basis for defining apply
+    let applyLift2 fOpt xOpt =
+        lift2 (fun f x -> f x) fOpt xOpt
+
+module List =
+    // The apply function for Lists
+    // [f;g] apply [x;y] becomes [f x; f y; g x; g y]
+    let apply (fList: ('a -> 'b) list) (xList: 'a list) =
+        [ for f in fList do
+          for x in xList do
+            yield f x]
+
+    let (<*>) = apply
+    let (<!>) = List.map
+
+    let lift2 f x y =
+        f <!> x <*> y
+
 // Two parameter example function
 let addPair x y = x + y
 
@@ -42,3 +60,51 @@ let addTriple x y z = x + y + z
 let addTripleOpt = Option.lift3 addTriple
 
 addTripleOpt (Some 1) (Some 2) (Some 3)
+
+// lift2 as a "combiner"
+// first parameter is how to combine the values
+Option.lift2 (+) (Some 2) (Some 3)
+Option.lift2 (*) (Some 2) (Some 3)
+
+
+
+//////Generic Way to combine Values/////////////
+
+// Tuple creation function
+let tuple x y = x,y
+
+// create generic combiner of options
+// with the tuple constructor baked in
+let combineOpt x y = Option.lift2 tuple x y
+
+// create generic combiner of lists
+// with tuple constructor baked in
+let combineList x y = List.lift2 tuple x y
+
+combineOpt (Some 1) (Some 2)
+combineList [1;2] [100;200]
+
+
+combineOpt (Some 1) (Some 2)
+|> Option.map (fun (x,y) -> x + y)
+
+combineList [1;2] [100;200]
+|> List.map (fun (x,y) -> x + y)
+
+combineOpt (Some 1) (Some 2)
+|> Option.map (fun (x,y) -> x * y)
+
+combineList [1;2] [100;200]
+|> List.map (fun (x,y) -> x * y)
+
+//////////One Sided Combiners//////////////
+// If you have two elevated values and want to discard the value from one side or the other
+
+let ( <* ) x y =
+    List.lift2 (fun left right -> left) x y
+
+let ( *> ) x y =
+    List.lift2 (fun left right -> right) x y
+
+[1;2] <* [3;4;5]
+[2;3] *> [3;4;5]
---
+++ FunAndProfitExamples/ElevatedWorldZip.fsx
@@ -0,0 +1,63 @@
+﻿// Zip function
+// Common Names: zip, zipWith, map2
+// Common Operators: <*> (in context of ZipList "World")
+// Combines 2 lists or other enumerables using a specified function
+// Signature: E<(a -> b -> c)> -> E<a> -> E<b> -> E<c> where E is an enumberable type or E<a> -> E<b> -> E<a,b> for tupled-combined version
+// Some data types may have more than one valid implementation of apply. In this implementation the corresponmding elements in each
+// list are processed at the same time, and then both lists are shifted to the next element
+
+
+// alternate "zip" implementation (not tail recursive)
+// [f;g] apply [x;y] becomes [f x; g y]
+// ('a -> 'b) list -> 'a list -> 'b list
+let rec zipList fList xList =
+    match fList,xList with
+    | [], _
+    | _,[] ->
+        // either side empty, then done
+        []
+    | (f::fTail),(x::xTail) ->
+        // new head + new tail
+        (f x) :: (zipList fTail xTail)
+
+let add10 x = x + 10
+let add20 x = x + 20
+let add30 x = x + 30
+
+let result =
+    let (<*>) = zipList
+    [add10; add20; add30] <*> [1;2;3]
+
+// zip list as combiner
+let add x y = x + y
+
+let resultAdd =
+    let (<*>) = zipList
+    [add;add] <*> [1;2] <*> [10;20]
+
+// ZipList world
+// Standard List world has apply and return. With out different version of apply we can create a differemy version of List world
+// called ZipList world. In ZipList world the apply function is implemented as above. ZipList world also has a completely different return 
+// implementation. In standard list world return is a list with a single element, in ZipList World it has to be an infinetly repeating value
+
+module ZipSeq =
+    // define return for ZipWorld
+    let retn x = Seq.initInfinite (fun _ -> x)
+
+    // define apply for ZipWorld
+    // ('a -> 'b) seq -> 'a seq -> 'b seq
+    let apply fSeq xSeq =
+        Seq.map2 (fun f x -> f x) fSeq xSeq
+
+    // define a sequence that is a combination of tweo others
+    let triangularNumbers =
+        let (<*>) = apply
+
+        let addAndDivideByTwo  x y = (x + y) / 2
+        let numbers = Seq.initInfinite id
+        let squareNumbers = Seq.initInfinite (fun i -> i * i)
+        (retn addAndDivideByTwo <*> numbers <*> squareNumbers)
+
+// evaluate first 10 elements
+// and display result
+ZipSeq.triangularNumbers |> Seq.take 10 |> List.ofSeq |> printfn "%A"
