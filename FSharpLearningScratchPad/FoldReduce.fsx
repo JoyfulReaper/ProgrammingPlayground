7ad87466f86037e3099b313a6d109111b51e6959
Kyle Givler <xvegan88x@gmail.com>
2023-09-22 08:12:14 -0400

    Another Map/Bind example

--- FSharpLearningScratchPad/FoldReduce.fsx
+++ FSharpLearningScratchPad/FoldReduce.fsx
@@ -1,2 +1,26 @@
-﻿module FoldReduce
+﻿let sumOfSquares numbers =
+    numbers |> List.fold (fun acc v -> acc + v * v) 0
 
+let r1 = [1..3] |> sumOfSquares
+
+// Reduce is a specific case of fold where the list is assumed to be non-empty
+// No initial seed value is needed
+
+let productOfAll (numbers: int list) : int =
+    numbers
+    |> List.reduce (fun a b -> a * b)
+
+let r2 = [1..5] |> productOfAll
+
+//////////////
+let shoppingCart = [("Item1", 10.0); ("Item2", 5.0); ("Item3", 15.0)]
+let c2 = []
+
+let calculateTotalPrice (cart : (string * float) list) =
+    cart
+    |> List.fold (fun acc (n, p) -> acc + p) 0.0
+
+shoppingCart |> calculateTotalPrice
+c2 |> calculateTotalPrice
+
+////////////
--- FSharpLearningScratchPad/MapBind.fsx
+++ FSharpLearningScratchPad/MapBind.fsx
@@ -1,2 +1,42 @@
-﻿module MapBind
+﻿open System
+
+type Thing =
+    {
+        Name: string
+        Color: string
+        Sanded: bool
+        Painted: bool
+        Tested: bool
+    }
+
+let paint color thing =
+    match Random.Shared.NextDouble() with
+    | x when x < 0.75 ->
+        Ok { thing with Color = color; Painted = true;}
+    | _ ->
+        Error "Failed to paint"
+
+let sand thing =
+    { thing with Sanded = true }
+
+let test thing =
+    match Random.Shared.NextDouble() with
+    | x when x < 0.75 ->
+            Ok { thing with Tested = true }
+    | _ ->
+            Error "Failed Testing"
+
+let paintSandAndTest color thing =
+    paint color thing
+    |> Result.map sand
+    |> Result.bind test
+
+let thing = { Name = "Thing"; Color = "Red"; Painted = false; Sanded = false; Tested = false }
+let thingResult = thing |> paintSandAndTest "Blue"
+
+match thingResult with
+| Ok t ->
+    printfn "Name: %s, Color: %s, Painted: %b, Sanded: %b, Tested: %b" t.Name t.Color t.Painted t.Sanded t.Tested
+| Error e ->
+    printfn "Error: %s" e
 
